98bec1827e64fae350f89b3e26faf952f161f5fc
Karin Flarup <karin.flarup@medieinstitutet.se>
2023-01-08 09:15:08 +0100

    added style to page-74.php

--- wp-content/themes/hatheme/page-74.php
+++ wp-content/themes/hatheme/page-74.php
@@ -1,18 +1,12 @@
 <?php get_header(); ?>
 <section class="section-wrapper">
     <div class="competence-heading-wrapper">
-        <div class="competence-heading">
-            <h2>Våra kompetensområde</h2>
-            <ul>
-                <li>Vi utför all förekommande målerikonservering och restaurering.</li>
-                <li>Nyutförande av förgyllning och dekorationsmålning.</li>
-                <li>Konsultuppdrag relaterade till byggnadsvård och konservering.</li>
-            </ul>
-            <p>Se alla arbeten vi utfört inom olika områden:</p>
-        </div>
-        <div class="competence-img">
-            <img alt="falsterbo kyrka målningar på altarskåpet" src="<?php echo get_theme_file_uri('/images/falsterbo kyrka målningar på altarskåpet.jpeg') ?>">
-        </div>
+        <h2>Våra kompetensområde</h2>
+        <ul class="competence-list">
+            <li>Vi utför all förekommande målerikonservering och restaurering.</li>
+            <li>Nyutförande av förgyllning och dekorationsmålning.</li>
+            <li>Konsultuppdrag relaterade till byggnadsvård och konservering.</li>
+        </ul>
     </div>
     </div>
     <div class="frontpage-category-div">
