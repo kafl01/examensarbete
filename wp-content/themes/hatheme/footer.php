dfabebb569373e2937822120a2876c05fa4c3a7b
Karin Flarup <karin.flarup@medieinstitutet.se>
2023-01-09 22:30:54 +0100

    updated footer with css

--- wp-content/themes/hatheme/footer.php
+++ wp-content/themes/hatheme/footer.php
@@ -3,19 +3,21 @@
 
 <nav class="navbar-footer">
     <div id="contact">
-        <img src="<?php echo get_theme_file_uri('/images/Logo2.png') ?>">
+        <div class="contact-logo">
+            <img src="<?php echo get_theme_file_uri('/images/Logo2_.png') ?>">
+        </div>
         <p>Herman Andersson</p>
         <p>070-653 12 44</p>
         <p>herman.andersson@telia.com</p>
     </div>
     <div class="footer-nav">
-        <p><a href="<?php echo get_permalink(3); ?>">Privacy Policy</a></p>
-        <p>
+        <p class="footer-nav-small"><a href="<?php echo get_permalink(3); ?>">Privacy Policy</a></p>
+        <p class="footer-nav-small">
             &copy; 2023 Konservator Herman Andersson AB
         </p>
     </div>
     <div>
-        <div>
+        <div class="footer-arrow">
             <a href="#up"><img src="<?php echo get_theme_file_uri('/images/straight-arrow.png') ?>"></a>
         </div>
     </div>
